d6fb45df8c57a5bfc345be7a51b15147121f2af2
SirJUST <theoperaking@yahoo.com>
2019-03-27 20:04:17 -0400

    add space to error log

--- AutomateWashingtonUploads/LogException.cs
+++ AutomateWashingtonUploads/LogException.cs
@@ -11,7 +11,7 @@ namespace AutomateWashingtonUploads
     {
         public void logException(Exception ex, Completion completion)
         {
-            string errorInfo = String.Format("The following completion encountered an{0} error:\r\n{1} | {2} | {3} | {4}", ex.GetType().ToString(), completion.course, completion.date, completion.license, completion.name);
+            string errorInfo = String.Format("The following completion encountered an {0} error:\r\n{1} | {2} | {3} | {4}", ex.GetType().ToString(), completion.course, completion.date, completion.license, completion.name);
             Logger logger = new Logger();
             StreamWriter sw = new StreamWriter(@"..\..\..\Logs\log_" + DateTime.Today.Month.ToString() + "-" + DateTime.Today.Day.ToString() + "-" + DateTime.Today.Year.ToString() + ".txt", true);
             logger.writeErrorsToLog(errorInfo, sw);
