e437502324424969e431f65f0ad5b317d1913f0f
SirJUST <theoperaking@yahoo.com>
2020-01-25 16:04:35 -0500

    Changes from December

--- AutomateWashingtonUploads/PlumbingCourses.cs
+++ AutomateWashingtonUploads/PlumbingCourses.cs
@@ -34,18 +34,24 @@ namespace AutomateWashingtonUploads
             "WA2018-185",
             "WA2018-398",
 
+            "WA2019-040",
+            "WA2019-041",
             "WA2019-155",
             "WA2019-156",
             "WA2019-157",
             "WA2019-158",
             "WA2019-159",
             "WA2019-229",
+            "WA2019-231",
             "WA2019-323",
             "WA2019-433",
             "WA2019-434",
             "WA2019-435",
             "WA2019-610",
-            "WA2019-040",
+            "WA2019-725",
+            "WA2019-726",
+            "WA2019-727",
+
             "WA2020-054",
             "WA2020-055"
         };
@@ -62,6 +68,9 @@ namespace AutomateWashingtonUploads
             {"WA2016-491", "WA2019-435" }, // Chapter 7 Sanitary Drains
             {"WA2016-587", "WA2019-434" }, // Chapter 6 Water Systems
             {"WA2016-741", "WA2019-610" }, // Understanding Ele. Wire Dia. 9.28.2019
+            {"WA2016-696", "WA2019-725" }, // 2015 UPC Chapter 2 Definitions Part 1
+            {"WA2016-695", "WA2019-726" }, // 2015 UPC Chapter 2 Definitions Part 2
+            {"WA2016-740", "WA2019-727" }, // Basic Electricity 12.2.2019
             {"WA2017-004", "WA2020-054" }, // 2015 UPC Chapter 9 Venting
             {"WA2017-116", "WA2020-055" }  // 2015 UPC Update 1.23.2019
         };
