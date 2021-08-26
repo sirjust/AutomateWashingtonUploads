95721da343f4a715ec2330bec1be7a2469ac0380
sirjust <theoperaking@yahoo.com>
2021-08-25 21:35:46 -0400

    Remove unnecessary ChromeDriver logging

--- AutomateWashingtonUploads/Dependency/DependencyContainer.cs
+++ AutomateWashingtonUploads/Dependency/DependencyContainer.cs
@@ -4,6 +4,7 @@ using AutomateWashingtonUploads.StaticData;
 using Ninject.Modules;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
 
 namespace AutomateWashingtonUploads.Dependency
 {
@@ -11,8 +12,14 @@ namespace AutomateWashingtonUploads.Dependency
     {
         public override void Load()
         {
+            // This should suppress Chrome logs where it says a usb device isn't functioning
+            var ls = new List<string> { "enable-logging" };
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddExcludedArguments(ls);
+            var driver = new ChromeDriver(options: chromeOptions);
+
             Bind<ILoginInfo>().To<LoginInfo>();
-            Bind<IWebDriver>().To<ChromeDriver>();
+            Bind<IWebDriver>().ToConstant(driver);
             Bind<IUploader>().To<Uploader>();
             Bind<IErrorHelper>().To<ErrorHelper>();
             Bind<ILogger>().To<Logger>();
