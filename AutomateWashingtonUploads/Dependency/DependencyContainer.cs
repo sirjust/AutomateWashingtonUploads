34b4129d8851d12f56f5b5e34da527e7ebb45a09
sirjust <theoperaking@yahoo.com>
2023-05-20 12:29:50 -0700

    Update to ChromeDriver 113 and handle incorrect ChromeDriver version exception

--- AutomateWashingtonUploads/Dependency/DependencyContainer.cs
+++ AutomateWashingtonUploads/Dependency/DependencyContainer.cs
@@ -4,6 +4,7 @@ using AutomateWashingtonUploads.StaticData;
 using Ninject.Modules;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 
 namespace AutomateWashingtonUploads.Dependency
@@ -13,19 +14,32 @@ namespace AutomateWashingtonUploads.Dependency
         public override void Load()
         {
             // This should suppress Chrome logs where it says a usb device isn't functioning
-            var ls = new List<string> { "enable-logging" };
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddExcludedArguments(ls);
-            var driver = new ChromeDriver(options: chromeOptions);
 
-            Bind<ILoginInfo>().To<LoginInfo>();
-            Bind<IWebDriver>().ToConstant(driver);
-            Bind<IUploader>().To<Uploader>();
-            Bind<IErrorHelper>().To<ErrorHelper>();
-            Bind<ILogger>().To<Logger>();
-            Bind<IEmailHelper>().To<EmailHelper>();
-            Bind<IValidationHelper>().To<ValidationHelper>();
-            Bind<ICompletionRepository>().To<CompletionRepository>();
+            try
+            {
+                var ls = new List<string> { "enable-logging" };
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.AddExcludedArguments(ls);
+                var driver = new ChromeDriver(options: chromeOptions);
+                Bind<ILoginInfo>().To<LoginInfo>();
+                Bind<IWebDriver>().ToConstant(driver);
+            }
+            catch
+            {
+                Console.WriteLine("ChromeDriver was unable to start. Contact support to update ChromeDriver.");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+            finally
+            {
+                Bind<IUploader>().To<Uploader>();
+                Bind<IErrorHelper>().To<ErrorHelper>();
+                Bind<ILogger>().To<Logger>();
+                Bind<IEmailHelper>().To<EmailHelper>();
+                Bind<IValidationHelper>().To<ValidationHelper>();
+                Bind<ICompletionRepository>().To<CompletionRepository>();
+            }
+
         }
     }
 }
